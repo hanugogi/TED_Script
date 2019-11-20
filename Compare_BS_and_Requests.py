5c7f8b82445f5fe872c2755da4d2e0fe20490e25
unknown <hyinwoojjang@gmail.com>
2019-11-20 11:45:23 +0900

    [Remove] Remove unecessary comment

--- Compare_BS_and_Requests.py
+++ Compare_BS_and_Requests.py
@@ -4,15 +4,15 @@ import requests
 
 url = "https://www.ted.com/talks/john_doerr_why_the_secret_to_success_is_setting_the_right_goals/transcript"
 response_urllib = urlopen(url)
-html_urllib = response_urllib.read() # 바이트코드 type으로 소스를 읽는다.
+html_urllib = response_urllib.read()
 
-soup_urllib = BeautifulSoup(html_urllib, "html.parser") # 파싱할 문서를 BeautifulSoup 클래스의 생성자에 넘겨주어 문서 개체를 생성, 관습적으로 soup 이라 부름
+soup_urllib = BeautifulSoup(html_urllib, "html.parser")
 
 p = open('response_urllib.txt', 'w')
 p.write(str(response_urllib))
 p.close()
 
-response_urllib.close() # urlopen을 진행한 후에는 close를 한다.
+response_urllib.close()
 
 p = open('html_urllib.txt', 'w')
 p.write(str(html_urllib))
--- Script_Downloader.py
+++ Script_Downloader.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 
 url = "https://www.ted.com/talks/john_doerr_why_the_secret_to_success_is_setting_the_right_goals/transcript"
 response_urllib = urlopen(url)
-html_urllib = response_urllib.read() # 바이트코드 type으로 소스를 읽는다.
+html_urllib = response_urllib.read()
 
-soup_urllib = BeautifulSoup(html_urllib, "html.parser") # 파싱할 문서를 BeautifulSoup 클래스의 생성자에 넘겨주어 문서 개체를 생성, 관습적으로 soup 이라 부름
+soup_urllib = BeautifulSoup(html_urllib, "html.parser")
 
 #with open('result.txt', 'w', encoding = 'UTF-8') as f:
 #	f.write(str(soup_urllib.find_all('a')))
