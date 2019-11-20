5b250a9caaf1d26a865bcae13178863db23d547c
unknown <hyinwoojjang@gmail.com>
2019-11-20 12:23:20 +0900

    [Add] Add print function

--- Quick_Start.py
+++ Quick_Start.py
@@ -7,32 +7,32 @@ html = response.read()
 
 soup = BeautifulSoup(html, "html.parser")
 
-soup.title
+print(soup.title)
 # <title>John Doerr: Why the secret to success is setting the right goals | TED Talk Subtitles and Transcript | TED</title>
 
-soup.title.name
+print(soup.title.name)
 # title
 
-soup.title.string
+print(soup.title.string)
 # John Doerr: Why the secret to success is setting the right goals | TED Talk Subtitles and Transcript | TED
 
-soup.title.parent.name
+print(soup.title.parent.name)
 # meta
 
-soup.p
+print(soup.p)
 # None
-soup.a
+print(soup.a)
 # <a href="https://enable-javascript.com/">Here's how</a>
 
-soup.a['href']
+print(soup.a['href'])
 # https://enable-javascript.com/
 
-soup.find_all('h3')
+print(soup.find_all('h3'))
 # [<h3 class="footer__title">
 # Programs &amp; initiatives
 # </h3>, <h3 class="footer__title">
 # Ways to get TED
 # </h3>, <h3 class="footer__title">Follow TED</h3>, <h3 class="footer__title">Our community</h3>, <h3 class="footer__title">Want personalized recommendations?</h3>, <h3 class="footer__title">Language Selector</h3>]
 
-soup.find(property="og:description")
+print(soup.find(property="og:description"))
 # <meta content="TED Talk Subtitles and Transcript: Our leaders and institutions are failing us, but it's not always because they're bad or unethical, says venture capitalist John Doerr -- often, it's simply because they're leading us toward the wrong objectives. In this practical talk, Doerr shows us how we can get back on track with &quot;Objectives and Key Results,&quot; or OKRs -- a goal-setting system that's been employed by the likes of Google, Intel and Bono to set and execute on audacious goals. Learn more about how setting the right goals can mean the difference between success and failure -- and how we can use OKRs to hold our leaders and ourselves accountable." property="og:description"/>
